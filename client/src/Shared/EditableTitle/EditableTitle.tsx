dd56829bdc766170f4364497d9c8dc912bf4e9fa
Emanuel-Constantin Turis <emanuelturis@gmail.com>
2020-08-27 12:08:58 +0100

    Add on click outside behaviour to EditableTitle component

--- client/src/Shared/EditableTitle/EditableTitle.tsx
+++ client/src/Shared/EditableTitle/EditableTitle.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect, createRef } from "react";
 import { FaPencilAlt, FaCheck } from "react-icons/fa";
 import { css } from "@emotion/core";
 import { Form } from "react-bootstrap";
+import OnClickOutside from "../OnClickOutside";
 
 interface Props {
   title: string;
@@ -19,58 +20,62 @@ const EditableTitle: React.FC<Props> = ({ title, updateTitle }) => {
   }, [editable]);
 
   const handleUpdateTitle = () => {
-    setEditable(false);
-    updateTitle(inputRef.current!.innerText);
+    setEditable(() => {
+      updateTitle(inputRef.current!.innerText);
+      return false;
+    });
   };
 
   return (
-    <div className="d-flex align-items-center">
-      <h1
-        ref={inputRef}
-        contentEditable={editable ? true : false}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" && editable) {
-            handleUpdateTitle();
-          }
-        }}
-        css={css`
-          outline: 0px solid transparent;
-        `}
-      >
-        {title}
-      </h1>
-      {editable ? (
-        <FaCheck
-          css={css`
-            margin-bottom: 8px;
-            margin-left: 5px;
-            cursor: pointer;
-            opacity: 0.8;
-            &:hover {
-              opacity: 1;
+    <OnClickOutside handler={() => handleUpdateTitle()} active={editable}>
+      <div className="d-flex align-items-center">
+        <h1
+          ref={inputRef}
+          contentEditable={editable ? true : false}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && editable) {
+              handleUpdateTitle();
             }
-          `}
-          onClick={() => {
-            handleUpdateTitle();
           }}
-        />
-      ) : (
-        <FaPencilAlt
           css={css`
-            margin-bottom: 8px;
-            margin-left: 5px;
-            cursor: pointer;
-            opacity: 0.8;
-            &:hover {
-              opacity: 1;
-            }
+            outline: 0px solid transparent;
           `}
-          onClick={() => {
-            setEditable(true);
-          }}
-        />
-      )}
-    </div>
+        >
+          {title}
+        </h1>
+        {editable ? (
+          <FaCheck
+            css={css`
+              margin-bottom: 8px;
+              margin-left: 5px;
+              cursor: pointer;
+              opacity: 0.8;
+              &:hover {
+                opacity: 1;
+              }
+            `}
+            onClick={() => {
+              handleUpdateTitle();
+            }}
+          />
+        ) : (
+          <FaPencilAlt
+            css={css`
+              margin-bottom: 8px;
+              margin-left: 5px;
+              cursor: pointer;
+              opacity: 0.8;
+              &:hover {
+                opacity: 1;
+              }
+            `}
+            onClick={() => {
+              setEditable(true);
+            }}
+          />
+        )}
+      </div>
+    </OnClickOutside>
   );
 };
 
