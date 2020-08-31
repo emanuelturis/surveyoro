c7213edbd7698100617eeafb8b6a8a2df39d5539
Emanuel-Constantin Turis <emanuelturis@gmail.com>
2020-08-31 15:14:23 +0100

    Modifify db env variables to url instead of multiple variables

--- server/src/db/config.ts
+++ server/src/db/config.ts
@@ -41,13 +41,7 @@ export = {
 
   production: {
     client: "postgresql",
-    connection: {
-      host: process.env.POSTGRES_HOST,
-      port: "5432",
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
